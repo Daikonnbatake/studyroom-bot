ae3f369de15939e83f5a253de750e7a607722698
= <=>
2021-06-01 21:10:18 +0000

    コマンドライン引数からtokenと開発者モードを指定できるようにした

--- main.py
+++ main.py
@@ -5,15 +5,53 @@ import json
 import time
 import discord
 import os
+import sys
 
-TOKEN = input('please token here: ')
+# コマンドライン引数を受け取る
+_arg = sys.argv[1:]
+
+# コマンドライン引数の検証
+try:
+    if len(_arg) == 1:
+        tokenCheck = _arg[0]
+        developMode = False
+
+    elif len(_arg) == 2:
+        if _arg[0] in ['True', 'False']:
+            developMode = _arg[0] == 'True'
+
+        else:
+            raise ValueError('Illegal argument')
+
+        tokenCheck = _arg[1]
+
+    else:
+        raise ValueError('Illegal argument')
+
+except:
+    print('---')
+    print('Please try again: python3 [path] [develop mode] [bot token]')
+    print('[path] = Path of this file')
+    print('[develop mode] = Enter true if booted in "develop mode", false otherwise. (This argument is optional argument. Default is "False".)')
+    print('[bot token] = Please enter to bot token.')
+    print('---')
+    exit()
+
+
+TOKEN = tokenCheck
+DEVELOPMODE = developMode
 INTENTS = discord.Intents.all()
 ROOT = os.path.dirname(__file__)
 
 with open(ROOT + '/bot.conf', 'r', encoding='utf-8') as f:
     CONFIG = json.loads(f.read())
 
-bot = commands.Bot(command_prefix='srb ', intents=INTENTS)
+if DEVELOPMODE:
+    bot = commands.Bot(command_prefix=CONFIG['bot']['developModeCommandPrefix'], intents=INTENTS)
+
+else:
+    bot = commands.Bot(command_prefix=CONFIG['bot']['developModeCommandPrefix'], intents=INTENTS)
+
 
 # Cog の読み込み
 def loadCogs():
@@ -45,7 +83,7 @@ async def on_voice_state_update(member, before, after):
 
     # 関係ないステータス更新は記録しない
     if beforeState == afterState: return
-    
+
     with open(ROOT+'/log/voiceStateLog/'+ guildID +'.csv', 'a', encoding='utf-8') as f:
         writer = csv.writer(f)
         # log/voiceStateLog.csv のフォーマットは [ユーザー名, 遷移前のボイチャ名, 遷移後のボイチャ名, 遷移した時間]
