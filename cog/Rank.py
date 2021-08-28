4a600e8daa5ad5e9f9c291d0d53a9c4524afe56f
= <=>
2021-08-28 22:38:00 +0000

    rankコマンド修正

--- cog/Rank.py
+++ cog/Rank.py
@@ -217,30 +217,28 @@ class Rank(commands.Cog):
     # mee6 の !rank みたいな画像を出す
     @commands.command()
     async def rank(self, ctx, user=None):
-        userName = ctx.author.name if user == None else user
-        user = ''
-
+        user = ctx.author.name if user == None else user
+        userid = 0
         if user == None:
-            user = ctx.author.id
-
+            userid = ctx.author.id
         else:
             for member in ctx.guild.members:
                 if member.name == user:
-                    user = member.id
+                    userid = ctx.author.id
 
         avatarURL = ctx.message.author.avatar_url
         
-        if not user in [member.id for member in ctx.guild.members]: return
+        if not user in [member.name for member in ctx.guild.members]: return
         
         # アバター取得
         for member in ctx.guild.members:
-            if member.id == user:
+            if member.name == user:
                 avatarURL = member.avatar_url
 
         baseImagePath = '%s/rank_base.png' % self.imagePath
         avatarMaskPath = '%s/avatar_mask.png' % self.imagePath
         savePath = '%s/%s.png' % (self.imagePath, ctx.author.id)
-        fixedRank = self._updateGuild(ctx.guild)[user]
+        fixedRank = self._updateGuild(ctx.guild)[userid]
         defaultColor = (185, 187, 190)
         rankColor = defaultColor
 
@@ -272,7 +270,7 @@ class Rank(commands.Cog):
         nomalFont = ImageFont.truetype(bfontPath, 14)
 
         image.paste(avatar, (21, 17))
-        draw.text((100, 18), userName, (255, 255, 255, 255), font=memberNameFont)
+        draw.text((100, 18), user, (255, 255, 255, 255), font=memberNameFont)
         draw.text((100, 48), fixedRank['nowRank'], rankColor, font=nomalFont)
         draw.text((20, 104), '過去7日間の自習時間: %s h' % (sum(fixedRank['activity'])//3600), defaultColor, font=nomalFont)
         draw.rectangle((102, 72, 250*(sum(fixedRank['activity'])/(3600*14)%1) + 102, 77), fill=rankColor)
